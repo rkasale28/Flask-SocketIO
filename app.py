613484df0c9626e229542b4cd97012fddf6a8010
rkasale28 <rohitkasale28@gmail.com>
2021-02-07 12:00:47 +0530

    Socket.io

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
 def home():
@@ -10,5 +12,17 @@ def home():
 def template():
     return render_template("index.html")
 
+@app.route('/sessions')
+def sessions():
+    return render_template('session.html')
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+    
 if __name__=="__main__":
-    app.run()
+    socketio.run(app, debug=True)
